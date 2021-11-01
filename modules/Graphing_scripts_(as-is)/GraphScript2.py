80887ff532497fcf5628cbab18711df174293ff8
EthanColbert8 <emcolbert2@gmail.com>
2021-11-01 02:39:35 -0400

    improved plotting functions in GraphScript2

--- modules/Graphing_scripts_(as-is)/GraphScript2.py
+++ modules/Graphing_scripts_(as-is)/GraphScript2.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-DON'T TRY TO INTEGRATE THIS WITH OTHER OBARO CODE IN CURRENT STATE
-This is the script that I used to create the scatterplot for the KAS presentation.
-I added it in case we want to adapt it and integrate into OBARO later.
-
 Created on Sat Oct 30 15:08:54 2021
+
 @author: Ethan
 """
 
@@ -12,45 +9,114 @@ from matplotlib import pyplot
 import xml.etree.cElementTree as ET
 #from xml.etree.ElementTree import XML, Element
 
-tree = ET.parse("seed1000-01.xml")
+tree = ET.parse("517-seed1000.xml")
 root = tree.getroot()
 
-dataFromXml = []
+dataFromXmlV1 = []
 
 index = 0
 for child in root:
     signal = int(child[4].attrib['value'])
     background = int(child[10].attrib['value'])
     
-    dataFromXml.append((index, signal, background))
+    dataFromXmlV1.append((index, signal, background))
+    
+    index += 1
+
+tree = ET.parse("5323-seed1000.xml")
+root = tree.getroot()
+
+dataFromXmlV3 = []
+
+index = 0
+for child in root:
+    signal = int(child[4].attrib['value'])
+    background = int(child[10].attrib['value'])
+    
+    dataFromXmlV3.append((index, signal, background))
     
     index += 1
 
 #NEXT FUNCTIONS ADAPTED FROM EARLIER GraphScript SCRIPT
 
-def scatterPlotAll():
-    index = []
-    signal = []
-    background = []
+def scatterPlot(dataSet1, version1, dataSet2=None, version2=None):
+    index1 = []
+    signal1 = []
+    background1 = []
     
-    for j in dataFromXml:
-        index.append(j[0])
-        signal.append(j[1])
-        background.append(j[2])
+    for j in dataSet1:
+        index1.append(j[0])
+        signal1.append(j[1])
+        background1.append(j[2])
     
     pyplot.figure(figsize=(8,6), dpi=100)
     
-    pyplot.scatter(index, signal, s=3, label="Atronomical Object Brightness", c="blue")
-    pyplot.scatter(index, background, s=3, label="Background Brightness", c="red")
+    pyplot.scatter(index1, signal1, s=3, label=("Object Brightness (" + str(len(index1)) + " objects in " + version1 + ")"), c="blue")
+    pyplot.scatter(index1, background1, s=3, label=("Background Brightness (" + str(len(index1)) + " objects in " + version1 + ")"), c="red")
     
+    numObjects = len(index1)
+    fileLabel = version1
     
-    pyplot.title("Surface Brightness of All Astronomical Objects")
-    pyplot.xlabel("Index of Astronomical Object (" + str(len(index)) + " objects)")
+    if dataSet2 is not None:
+        index2 = []
+        signal2 = []
+        background2 = []
+        
+        for j in dataSet2:
+            index2.append(j[0] + numObjects)
+            signal2.append(j[1])
+            background2.append(j[2])
+        
+        pyplot.scatter(index2, signal2, s=3, label=("Object Brightness (" + str(len(index2)) + " objects in " + version2 + ")"), c="green")
+        pyplot.scatter(index2, background2, s=3, label=("Background Brightness (" + str(len(index2)) + " objects in " + version2 + ")"), c="fuchsia")
+        
+        numObjects += len(index2)
+        fileLabel = "both"
+    
+    pyplot.title("Surface Brightness of Brightest Astronomical Object in Each FITS Image (seed 1000)")
+    pyplot.xlabel("Index of Astronomical Object (" + str(numObjects) + " objects)")
     pyplot.ylabel("Surface Brightness")
     pyplot.xlim(xmin=0)
     pyplot.ylim(0, 1800000)
     pyplot.yticks([0, 200000, 400000, 600000, 800000, 1000000, 1200000, 1400000, 1600000], [0, 200000, 400000, 600000, 800000, 1000000, 1200000, 1400000, 1600000])
     pyplot.legend()
     
-    pyplot.savefig("scatter_all.png", dpi='figure')
+    pyplot.savefig("plot_images\scatter_" + fileLabel + ".png", dpi='figure')
+    pyplot.show()
+
+def plotSignalHist(dataSet, binSize, version, includeBackground=False):
+    
+    signalValues = []
+    backgroundValues = []
+    for j in dataSet:
+        signalValues.append(j[1])
+    
+    if includeBackground:
+        for j in dataSet:
+            backgroundValues.append(j[2])
+    
+    binList = []
+    count = 0
+    while count <= 1600000:
+        binList.append(count)
+        count += binSize
+    
+    pyplot.figure(figsize=(8,6), dpi=100)
+    
+    pyplot.hist(signalValues, bins = binList, label=("Object Surface Brightness (" + str(len(dataSet)) + " Objects)"))
+    if includeBackground:
+        pyplot.hist(backgroundValues, bins = binList, label="Background Surface Brightness (" + str(len(dataSet)) + " Objects)", alpha=0.5)
+    
+    pyplot.title("Distribution of Surface Brightness (" + version + " - seed1000)")
+    pyplot.xlabel("Surface Brightness of Brightest Astronomical Object in Each FITS Image")
+    pyplot.ylabel("No. of Astronomical Objects / " + str(binSize) + " (SBU)")
+    pyplot.xlim(0, 1600000)
+    pyplot.ylim(ymin=0)
+    pyplot.xticks([0, 200000, 400000, 600000, 800000, 1000000, 1200000, 1400000, 1600000], ["0", "2x10^5", "4x10^5", "6x10^5", "8x10^5", "1x10^6", "1.2x10^6", "1.4x10^6", "1.6x10^6"])
+    pyplot.legend()
+    
+    if (includeBackground):
+        pyplot.savefig("plot_images\histogram_" + version + "_" + str(binSize) + "_both.png", dpi='figure')
+    else:
+        pyplot.savefig("plot_images\histogram_" + version + "_" + str(binSize) + "_object.png", dpi='figure')
     pyplot.show()
